ff2efba9b462a54607369868117f61dc4138efa2
Harsh Jeswani <harshjeswanitech95722@gmail.com>
2026-01-27 23:57:07 +0530

    Implement HelloWorld contract

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,2 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract HelloWorld {
+    string public message;
+
+    constructor() {
+        message = "Hello, Web3!";
+    }
+
+    function updateMessage(string memory _newMessage) public {
+        message = _newMessage;
+    }
+}
 
 
