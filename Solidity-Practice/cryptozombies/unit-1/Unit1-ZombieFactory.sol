685f6510382ad9410057c941562f70add3a4e551
Harsh Jeswani <harshjeswanitech95722@gmail.com>
2026-01-29 09:13:29 +0530

    Emitted `NewZombie` event on zombie creation
    Added event emission for new zombies created.

--- Solidity-Practice/cryptozombies/unit-1/Unit1-ZombieFactory.sol
+++ Solidity-Practice/cryptozombies/unit-1/Unit1-ZombieFactory.sol
@@ -2,6 +2,8 @@ pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
+ event NewZombie(uint zombieId, string name, uint dna);
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
     struct Zombie {
@@ -11,7 +13,8 @@ contract ZombieFactory {
      Zombie[] public zombies;
 
     function _createZombie(string memory _name, uint _dna) private {
-             zombies.push(Zombie(_name, _dna));
+              uint id = zombies.push(Zombie(_name, _dna)) - 1;
+              emit NewZombie(id, _name, _dna);
 }
     function _generateRandomDna(string memory _str) private view returns (uint) {
          uint rand = uint(keccak256(abi.encodePacked(_str)));
